af2fa23e5b811a9499b2bf1b0fd832219f4fe125
James Xia <113453761+jamsxia@users.noreply.github.com>
2024-07-30 16:39:53 -0400

    Add files via upload
    finish pickling, test epoch with 20 an 60

--- assignment4/mlp.py
+++ assignment4/mlp.py
@@ -1,57 +1,15 @@
 
 from torch.utils.data import Dataset, DataLoader
-import numpy as np
-import os
-from PIL import Image
-from torchvision import transforms
-
-labels = np.array([2, 0, 4, 1])
-np.save('labels',labels)
-labels_1 = np.load('labels.npy')
-labels_1
-## data equipment
-class ImageDataset(Dataset):
-  def __init__(self, img_dir, label_file):
-    super(ImageDataset, self).__init__()
-    self.img_dir = img_dir
-    self.labels = torch.tensor(np.load(label_file, allow_pickle=True))
-    self.transforms = transforms.ToTensor()
-  
-  def __getitem__(self, idx):
-    img_pth = os.path.join(self.img_dir, "img_{}.jpg".format(idx))
-    img = Image.open(img_pth)
-    img = self.transforms(img).flatten()
-    label = self.labels[idx]
-    return {"data":img, "label":label}
-  
-  def __len__(self):
-    return len(self.labels)
-
-train_dataset = ImageDataset(img_dir='./data/train/',
-                             label_file='./data/train/labels.npy')
-
-train_loader = DataLoader(train_dataset, 
-                          batch_size=4, 
-                          shuffle=True)
-
-for minibatch in train_loader:
-  data, labels = minibatch['data'], minibatch['label']
-  print(data)
-  print(labels)
-
-##building MNIST classifer in Pytorch
-
-import matplotlib.pyplot as plt
+from torchvision.datasets import MNIST
+from torchvision.transforms import ToTensor
 import torch
 from torch import optim
 import torch.nn as nn
-from torch.utils.data import Dataset, DataLoader
-from torchvision.datasets import MNIST
-from torchvision.transforms import ToTensor
+from mlp_train import train
+from mlp_test import test
+
 # For reproducability
 torch.manual_seed(0)
-
-
 class BaseClassifier(nn.Module):
   def __init__(self, in_dim, feature_dim, out_dim):
     super(BaseClassifier, self).__init__()
@@ -64,6 +22,15 @@ class BaseClassifier(nn.Module):
   def forward(self, x):
     return self.classifier(x)
     
+# Instantiate model, optimizer, and hyperparameter(s)
+in_dim, feature_dim, out_dim = 784, 256, 10
+lr=1e-3
+loss_fn = nn.CrossEntropyLoss()
+epochs=60
+classifier = BaseClassifier(in_dim, feature_dim, out_dim)
+optimizer = optim.SGD(classifier.parameters(), lr=lr)
+
+
 
 # Load in MNIST dataset from PyTorch
 train_dataset = MNIST(".", train=True, 
@@ -74,70 +41,10 @@ train_loader = DataLoader(train_dataset,
                           batch_size=64, shuffle=True)
 test_loader = DataLoader(test_dataset, 
                          batch_size=64, shuffle=False)
-     
 
 
-# Instantiate model, optimizer, and hyperparameter(s)
-in_dim, feature_dim, out_dim = 784, 256, 10
-lr=1e-3
-loss_fn = nn.CrossEntropyLoss()
-epochs=40
-classifier = BaseClassifier(in_dim, feature_dim, out_dim)
-optimizer = optim.SGD(classifier.parameters(), lr=lr)
-
-def train(classifier=classifier,
-          optimizer=optimizer,
-          epochs=epochs,
-          loss_fn=loss_fn):
-
-  classifier.train()
-  loss_lt = []
-  for epoch in range(epochs):
-    running_loss = 0.0
-    for minibatch in train_loader:
-      data, target = minibatch
-      data = data.flatten(start_dim=1)
-      out = classifier(data)
-      computed_loss = loss_fn(out, target)
-      computed_loss.backward()
-      optimizer.step()
-      optimizer.zero_grad()
-      # Keep track of sum of loss of each minibatch
-      running_loss += computed_loss.item()
-    loss_lt.append(running_loss/len(train_loader))
-    print("Epoch: {} train loss: {}".format(epoch+1, running_loss/len(train_loader)))
-
-  plt.plot([i for i in range(1,epochs+1)], loss_lt)
-  plt.xlabel("Epoch")
-  plt.ylabel("Training Loss")
-  plt.title(
-      "MNIST Training Loss: optimizer {}, lr {}".format("SGD", lr))
-  plt.show()
-
-  # Save state to file as checkpoint
-  torch.save(classifier.state_dict(), 'mnist.pt')
-     
 
 
-def test(classifier=classifier, 
-          loss_fn = loss_fn):
-  classifier.eval()
-  accuracy = 0.0
-  computed_loss = 0.0
-
-  with torch.no_grad():
-      for data, target in test_loader:
-          data = data.flatten(start_dim=1)
-          out = classifier(data)
-          _, preds = out.max(dim=1)
-
-          # Get loss and accuracy
-          computed_loss += loss_fn(out, target)
-          accuracy += torch.sum(preds==target)
-          
-      print("Test loss: {}, test accuracy: {}".format(
-          computed_loss.item()/(len(test_loader)*64), accuracy*100.0/(len(test_loader)*64)))
-
-
-train()
-test()
+train(train_loader,classifier,optimizer,epochs,loss_fn,lr)
+out=BaseClassifier(in_dim, feature_dim, out_dim)
+test(test_loader, classifier, loss_fn,out)
